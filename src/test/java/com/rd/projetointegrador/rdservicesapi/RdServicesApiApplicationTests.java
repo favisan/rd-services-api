e85cf0d29678776caf53eefa05ffbf9c4c48d04b
Fabio Vieira <vieira578@gmail.com>
2021-01-28 18:55:14 -0300

    Commit inicial

---
+++ src/test/java/com/rd/projetointegrador/rdservicesapi/RdServicesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rd.projetointegrador.rdservicesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdServicesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
