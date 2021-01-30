73443e7ee067910661ec49fe5cc3d5974e195212
Maina <fisiomainalex@gmail.com>
2021-01-29 22:49:55 -0300

    criacao da ContatoEntity e TipoConfirmacaoEntity

---
+++ src/main/java/com/rd/projetointegrador/rdservicesapi/entity/ContatoEntity.java
@@ -0,0 +1,40 @@
+package com.rd.projetointegrador.rdservicesapi.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigInteger;
+
+@Entity
+@Table(name = "TB_CONTATO")
+@Data
+public class ContatoEntity implements Serializable {
+
+    @Id
+    @GeneratedValue
+    @Column(name="ID_CONTATO")
+    private BigInteger idContato;
+
+    @Column(name="ID_USUARIO")
+    private BigInteger idUsuario;
+
+    @Column(name="ID_LOJA")
+    private BigInteger idLoja;
+
+    @Column(name="ID_TIPO_CONTATO")
+    private BigInteger idTipoContato;
+
+    @Column(name="NR_DDI")
+    private String nrDdi;
+
+    @Column(name="NR_DDD")
+    private String nrDdd;
+
+    @Column(name="NR_RAMAL")
+    private String nrRamal;
+
+    @Column(name="DS_CONTATO")
+    private String dsContato;
+
+}
