605cac4b8531d13ced49b1e3a5f2e49a89caaed2
Maina <fisiomainalex@gmail.com>
2021-02-02 17:51:11 -0300

    delete metodo de especialidades

--- src/main/java/com/rd/projetointegrador/rdservicesapi/repository/EspMedRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rd.projetointegrador.rdservicesapi.repository;
-
-import com.rd.projetointegrador.rdservicesapi.entity.EspMedEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.math.BigInteger;
-
-public interface EspMedRepository extends JpaRepository <EspMedEntity, BigInteger>{
-}
