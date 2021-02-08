f4ab973f8b0aa880af19a21111972d9147ba88dd
Giovanna Albuquerque <ghb.albuquerque@gmail.com>
2021-02-08 14:29:57 -0300

    area cliente

---
+++ src/main/java/com/rd/projetointegrador/rdservicesapi/controller/ClienteController.java
@@ -0,0 +1,49 @@
+package com.rd.projetointegrador.rdservicesapi.controller;
+
+import com.rd.projetointegrador.rdservicesapi.dto.FormularioCadastro;
+import com.rd.projetointegrador.rdservicesapi.dto.FormularioMeusDados;
+import com.rd.projetointegrador.rdservicesapi.dto.InputCliente;
+import com.rd.projetointegrador.rdservicesapi.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.websocket.server.PathParam;
+import java.math.BigInteger;
+
+public class ClienteController {
+
+    @Autowired
+    private ClienteService service;
+
+    //PÁGINA DE CADASTRO
+    @GetMapping("/cliente/cadastro")
+    public ResponseEntity getFormularioCadastro(){
+        FormularioCadastro formularioCadastro = service.getFormularioCadastro();
+        return ResponseEntity.status(HttpStatus.OK).body(formularioCadastro);
+    }
+    //PÁGINA MEUS-DADOS
+    @GetMapping("/cliente/meus-dados/{id}")
+    public ResponseEntity getFormularioMeusDados(@PathParam("idUsuario") BigInteger idUsuario){
+        FormularioMeusDados formularioMeusDados = service.getFormularioMeusDados(idUsuario);
+        return ResponseEntity.status(HttpStatus.OK).body(formularioMeusDados);
+    }
+
+    //PÁGINA ÁREA CLIENTE
+//    @GetMapping("/cliente/area-cliente/{id}")
+//    public ResponseEntity getFormularioMeusDados(@PathParam("idUsuario") BigInteger idUsuario){
+//        FormularioMeusDados formularioMeusDados = service.getFormularioMeusDados(idUsuario);
+//        return ResponseEntity.status(HttpStatus.OK).body(formularioMeusDados);
+//    }
+
+
+
+    //POST PÁGINA DE CADASTRO (Novo Usuario + Login + Contato + Cartao + Contrato)
+    @PostMapping("/cliente/cadastro")
+    public ResponseEntity cadastrarUsuario(@RequestBody InputCliente inputCliente) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.cadastrarCliente(inputCliente));
+    }
+}
---
+++ src/main/java/com/rd/projetointegrador/rdservicesapi/service/ClienteService.java
@@ -0,0 +1,142 @@
+package com.rd.projetointegrador.rdservicesapi.service;
+
+import com.rd.projetointegrador.rdservicesapi.dto.FormularioCadastro;
+import com.rd.projetointegrador.rdservicesapi.dto.FormularioMeusDados;
+import com.rd.projetointegrador.rdservicesapi.dto.InputCliente;
+import com.rd.projetointegrador.rdservicesapi.dto.Usuario;
+import com.rd.projetointegrador.rdservicesapi.entity.CartaoEntity;
+import com.rd.projetointegrador.rdservicesapi.entity.ContratoEntity;
+import com.rd.projetointegrador.rdservicesapi.entity.LoginUsuarioEntity;
+import com.rd.projetointegrador.rdservicesapi.entity.UsuarioEntity;
+import com.rd.projetointegrador.rdservicesapi.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.math.BigInteger;
+import java.util.List;
+
+@Service
+public class ClienteService {
+
+    //repositories
+    @Autowired
+    private UsuarioRepository repository;
+    @Autowired private GeneroRepository generoRepository;
+    @Autowired private TipoUsuarioRepository tipoUsuarioRepository;
+    @Autowired private LoginUsuarioRepository loginUsuarioRepository;
+    @Autowired private ContratoRepository contratoRepository;
+    @Autowired private CartaoRepository cartaoRepository;
+
+    //services
+    //@Autowired private UfService ufService;
+    @Autowired private UsuarioService usuarioService;
+    @Autowired private GeneroService generoService;
+    @Autowired private PlanosService planosService;
+    @Autowired private ContratoService contratoService;
+    @Autowired private LoginUsuarioService luService;
+    @Autowired private CartaoService cartaoService;
+    //@Autowired private EnderecoService enderecoService;
+
+    //TODO: CADASTRO DE USUÁRIO - TELA DE CADASTRO
+    //TODO: faltando contato e endereço usuário
+    @Transactional
+    public String cadastrarCliente(InputCliente inputUsuario){
+        UsuarioEntity usuarioEntity = new UsuarioEntity();
+        LoginUsuarioEntity loginUsuarioEntity = new LoginUsuarioEntity();
+        ContratoEntity contratoEntity = new ContratoEntity();
+        CartaoEntity cartaoEntity= new CartaoEntity();
+
+        //VALIDAR CPF
+        String cpf = usuarioEntity.getNrCpf();
+        List<UsuarioEntity> usuarioExistente = repository.findByNrCpf(cpf);
+
+        //VALIDAR E-MAIL
+        String email = loginUsuarioEntity.getDsEmail();
+        LoginUsuarioEntity loginExistente = loginUsuarioRepository.findByDsEmail(email);
+
+
+        if(usuarioExistente.isEmpty() && loginExistente == null) {
+
+            //Passando dados do Usuário
+            usuarioEntity = usuarioService.conversaoUsuarioEntity(inputUsuario.getUsuario(), usuarioEntity);
+            usuarioEntity = repository.save(usuarioEntity);
+            BigInteger novoId = usuarioEntity.getIdUsuario();
+
+            //Entidade LoginUsuario
+            inputUsuario.getLoginUsuario().setIdUsuario(novoId);
+            loginUsuarioEntity = luService.conversaoLoginUsuarioEntity(inputUsuario.getLoginUsuario(), loginUsuarioEntity);
+            loginUsuarioRepository.save(loginUsuarioEntity);
+
+            //Entidade Contrato
+            inputUsuario.getContrato().setIdUsuario(novoId);
+            contratoEntity = contratoService.conversaoContratoEntity(inputUsuario.getContrato(), contratoEntity);
+            contratoRepository.save(contratoEntity);
+
+            //Entidade Cartao
+            inputUsuario.getCartao().setIdUsuario(novoId);
+            cartaoEntity = cartaoService.conversaoCartaoEntity(inputUsuario.getCartao(), cartaoEntity);
+            cartaoRepository.save(cartaoEntity);
+
+            //Entidade Contato
+            //TODO: fazer relacao com contatoService para cadastrar telefone
+
+        /*
+        //contato
+        private String ddd;
+        private String celular;*/
+
+            return "Usuário cadastrado com sucesso";
+        }
+
+        return "Erro. Usuário já cadastrado.";
+    }
+
+    //TELA DE CADASTRO - GET
+    //TODO: faltando recuperar lista de Ufs para o Form
+    public FormularioCadastro getFormularioCadastro() {
+        FormularioCadastro formularioCadastro = new FormularioCadastro();
+
+        //formularioCadastro.setUfs(ufService.getUfsDTO());
+        formularioCadastro.setGenero(generoService.getGenerosDTO());
+        formularioCadastro.setPlanos(planosService.getPlanosDTO());
+        return formularioCadastro;
+    }
+
+    //TELA MEUS DADOS - GET
+    //TODO: faltando dados relativos a contato, uf
+    public FormularioMeusDados getFormularioMeusDados(BigInteger id) {
+        FormularioCadastro formularioCadastro = getFormularioCadastro();
+        FormularioMeusDados formularioMeusDados = new FormularioMeusDados();
+
+        Boolean teste = repository.existsById(id);
+        if(teste) {
+            //buscar entities
+            UsuarioEntity usuarioEntity = usuarioService.getUsuario(id);
+            Usuario usuario = new Usuario();
+            usuario = usuarioService.conversaoUsuarioDTO(usuarioEntity, usuario);
+
+            //buscar e-mail de login
+            String email = loginUsuarioRepository.findOneByIdUsuario(id).getDsEmail();
+
+            //buscar idPlano no contrato
+            List<ContratoEntity> contratosEntities = contratoRepository.findByUsuarioOrderByDtVigencia(usuarioEntity);
+            BigInteger idPlanoVigente = contratosEntities.get(0).getPlanosEntity().getIdPlano();
+
+            //buscar lista de contatos
+            //Optional<ContatoEntity> optional = repository.findByIdUsuario(id);
+
+            formularioMeusDados.setUsuario(usuario);
+            formularioMeusDados.setDsEmail(email);
+            //formularioMeusDados.setContato();
+            formularioMeusDados.setIdPlano(idPlanoVigente);
+
+        }
+
+        //formularioMeusDados.setUfs(formularioCadastro.getUfs());
+        formularioMeusDados.setGenero(formularioCadastro.getGenero());
+        formularioMeusDados.setPlanos(formularioCadastro.getPlanos());
+
+        return formularioMeusDados;
+    }
+}
