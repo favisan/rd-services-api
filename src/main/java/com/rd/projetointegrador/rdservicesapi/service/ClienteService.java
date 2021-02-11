c93687864b6ed618b027d2d06ddd3a7b45143dea
Giovanna Albuquerque <ghb.albuquerque@gmail.com>
2021-02-11 16:46:23 -0300

    uf service

--- src/main/java/com/rd/projetointegrador/rdservicesapi/service/ClienteService.java
+++ src/main/java/com/rd/projetointegrador/rdservicesapi/service/ClienteService.java
@@ -1,16 +1,14 @@
 package com.rd.projetointegrador.rdservicesapi.service;
 
 import com.rd.projetointegrador.rdservicesapi.dto.*;
-import com.rd.projetointegrador.rdservicesapi.entity.CartaoEntity;
-import com.rd.projetointegrador.rdservicesapi.entity.ContratoEntity;
-import com.rd.projetointegrador.rdservicesapi.entity.LoginUsuarioEntity;
-import com.rd.projetointegrador.rdservicesapi.entity.UsuarioEntity;
+import com.rd.projetointegrador.rdservicesapi.entity.*;
 import com.rd.projetointegrador.rdservicesapi.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,9 +22,12 @@ public class ClienteService {
     @Autowired private LoginUsuarioRepository loginUsuarioRepository;
     @Autowired private ContratoRepository contratoRepository;
     @Autowired private CartaoRepository cartaoRepository;
+    @Autowired private ContatoRepository contatoRepository;
+    @Autowired private TipoContatoRepository tipoContatoRepository;
+    @Autowired private EnderecoRepository enderecoRepository;
 
     //services
-    //@Autowired private UfService ufService;
+    @Autowired private UfService ufService;
     @Autowired private UsuarioService usuarioService;
     @Autowired private GeneroService generoService;
     @Autowired private PlanosService planosService;
@@ -34,16 +35,15 @@ public class ClienteService {
     @Autowired private LoginUsuarioService luService;
     @Autowired private CartaoService cartaoService;
     @Autowired private LembreteService lembreteService;
-    //@Autowired private EnderecoService enderecoService;
+    @Autowired private EnderecoService enderecoService;
 
-    //TODO: CADASTRO DE USUÁRIO - TELA DE CADASTRO
-    //TODO: faltando contato e endereço usuário
     @Transactional
     public String cadastrarCliente(InputCliente inputUsuario){
         UsuarioEntity usuarioEntity = new UsuarioEntity();
         LoginUsuarioEntity loginUsuarioEntity = new LoginUsuarioEntity();
         ContratoEntity contratoEntity = new ContratoEntity();
         CartaoEntity cartaoEntity= new CartaoEntity();
+        //List<EnderecoEntity> enderecosEntities = new ArrayList<>();
 
         //VALIDAR CPF
         String cpf = usuarioEntity.getNrCpf();
@@ -77,12 +77,18 @@ public class ClienteService {
             cartaoRepository.save(cartaoEntity);
 
             //Entidade Contato
-            //TODO: fazer relacao com contatoService para cadastrar telefone
+            ContatoEntity contatoEntity = new ContatoEntity();
+            contatoEntity.setIdUsuario(novoId);
+            contatoEntity.setNrDdd(inputUsuario.getDdd());
+            contatoEntity.setDsContato(inputUsuario.getCelular());
+            contatoEntity.setTipoContato(tipoContatoRepository.findById(new BigInteger("3")).get());
 
-        /*
-        //contato
-        private String ddd;
-        private String celular;*/
+            //Entidade Endereço
+//          List<Endereco> enderecos = inputUsuario.getUsuario().getEnderecos();
+//          enderecosEntities = enderecoService.conversaoEnderecosEntities(enderecos,enderecosEntities);
+//          for(EnderecoEntity enderecoEntity: enderecosEntities) {
+//          enderecoRepository.save(enderecoEntity);
+//          }
 
             return "Usuário cadastrado com sucesso";
         }
@@ -91,18 +97,17 @@ public class ClienteService {
     }
 
     //TELA DE CADASTRO - GET
-    //TODO: faltando recuperar lista de Ufs para o Form
     public FormularioCadastro getFormularioCadastro() {
         FormularioCadastro formularioCadastro = new FormularioCadastro();
 
-        //formularioCadastro.setUfs(ufService.getUfsDTO());
+        formularioCadastro.setUfs(ufService.getUfsDTO());
         formularioCadastro.setGenero(generoService.getGenerosDTO());
         formularioCadastro.setPlanos(planosService.getPlanosDTO());
         return formularioCadastro;
     }
 
     //TELA MEUS DADOS - GET
-    //TODO: faltando dados relativos a contato, uf
+    //TODO: faltando dados relativos a contato
     public FormularioMeusDados getFormularioMeusDados(BigInteger id) {
         FormularioCadastro formularioCadastro = getFormularioCadastro();
         FormularioMeusDados formularioMeusDados = new FormularioMeusDados();
@@ -122,16 +127,16 @@ public class ClienteService {
             BigInteger idPlanoVigente = contratosEntities.get(0).getPlanosEntity().getIdPlano();
 
             //buscar lista de contatos
-            //Optional<ContatoEntity> optional = repository.findByIdUsuario(id);
+            List<ContatoEntity> contatosEntities = contatoRepository.findByIdUsuario(id);
 
             formularioMeusDados.setUsuario(usuario);
             formularioMeusDados.setDsEmail(email);
-            //formularioMeusDados.setContato();
+            formularioMeusDados.setContatos(null); //TODO!!!
             formularioMeusDados.setIdPlano(idPlanoVigente);
 
         }
 
-        //formularioMeusDados.setUfs(formularioCadastro.getUfs());
+        formularioMeusDados.setUfs(formularioCadastro.getUfs());
         formularioMeusDados.setGenero(formularioCadastro.getGenero());
         formularioMeusDados.setPlanos(formularioCadastro.getPlanos());
 
