24e7c3d827c6af7c4ed416578616381a32b7a752
Maina <fisiomainalex@gmail.com>
2021-02-15 17:14:57 -0300

    alteracao na controller do tipo de commit

--- src/main/java/com/rd/projetointegrador/rdservicesapi/controller/TipoConsultaController.java
+++ src/main/java/com/rd/projetointegrador/rdservicesapi/controller/TipoConsultaController.java
@@ -6,14 +6,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigInteger;
 
+@RestController
 public class TipoConsultaController {
     @Autowired private TipoConsultaService service;
 
     //Grupo2 - Consultar tipo de consulta pela id
-    @GetMapping("/tipoConsulta/{idAgConsulta}")
+    @GetMapping("/tipoConsulta/{idTipoConsulta}")
     public ResponseEntity listarTipoConsultaPorId(@PathVariable("idTipoConsulta") BigInteger idTipoConsulta){
         return ResponseEntity.status(HttpStatus.OK).body(service.getTipoConsultabyId(idTipoConsulta));
     }
