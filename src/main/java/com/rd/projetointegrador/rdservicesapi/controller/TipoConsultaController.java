dd7c17dc0ba5972e06f433b3dbaaf88bc1244997
ThayanaAraujo <tharaujo@rd.com.br>
2021-02-16 11:53:03 -0300

    inclusão anotação restcontroller

--- src/main/java/com/rd/projetointegrador/rdservicesapi/controller/TipoConsultaController.java
+++ src/main/java/com/rd/projetointegrador/rdservicesapi/controller/TipoConsultaController.java
@@ -6,9 +6,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigInteger;
 
+@RestController
 public class TipoConsultaController {
     @Autowired private TipoConsultaService service;
 
