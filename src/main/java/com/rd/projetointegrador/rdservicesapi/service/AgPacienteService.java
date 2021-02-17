dae1068aebb717cd67a0f9661c467fc86de8b6fd
Maina <fisiomainalex@gmail.com>
2021-02-17 15:04:21 -0300

    mudanca na ag paciennte

--- src/main/java/com/rd/projetointegrador/rdservicesapi/service/AgPacienteService.java
+++ src/main/java/com/rd/projetointegrador/rdservicesapi/service/AgPacienteService.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class AgPacienteService {
+public class git AgPacienteService {
 
     @Autowired private AgPacienteRepository repository;
     @Autowired private UsuarioRepository usuarioRepository;
